68f09eefefaf0b3d2184460642c27b2c5ef2a823
Boreas <jaywu163.com>
2025-06-21 06:20:45 +0800

    update binance trade settings

--- exchange/binance_settings.py
+++ exchange/binance_settings.py
@@ -1,5 +1,5 @@
-tick_size = {'BTCUSDT': 1, 'ETHUSDT': 2, 'SOLUSDT': 2, 'BTCUSDC': 1, 'ETHUSDC': 2, 'SOLUSDC': 2}
+tick_size = {'BTCUSDT': 1, 'ETHUSDT': 2, 'SOLUSDT': 2, 'BTCUSDC': 1, 'ETHUSDC': 2, 'SOLUSDC': 2, 'SUIUSDC': 4}
 
-step_size = {'BTCUSDT': 4, 'ETHUSDT': 3, 'SOLUSDT': 2, 'BTCUSDC': 4, 'ETHUSDC': 3, 'SOLUSDC': 2}
+step_size = {'BTCUSDT': 4, 'ETHUSDT': 3, 'SOLUSDT': 2, 'BTCUSDC': 4, 'ETHUSDC': 3, 'SOLUSDC': 2, 'SUIUSDC': 1}
 
 # todo: Automatically fetch the latest infos from exchange
